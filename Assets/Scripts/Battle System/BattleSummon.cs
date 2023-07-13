d7417d203a71e4d30dae98fa174b2394bf61f9c0
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2023-07-13 18:28:30 +0800

    07/13/23 Working on the battle system to support multiple character. Might handle turn order first

---
+++ Assets/Scripts/Battle System/BattleSummon.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSummon : MonoBehaviour
+{
+    [SerializeField] private List<BattlersInformation> _playerCharacters;
+    [SerializeField] private List<BattlersInformation> _enemyCharacters;
+
+    [SerializeField] private Transform _playerBattleStation;
+    [SerializeField] private Transform _enemyBattleStation;
+
+    private void Start()
+    {
+        InitializeBattlers();
+    }
+
+    private void InitializeBattlers()
+    {
+        SummonCharacters(_playerCharacters, _playerBattleStation);
+        SummonCharacters(_enemyCharacters, _enemyBattleStation);
+    }
+
+    private static void SummonCharacters(List<BattlersInformation> characters, Transform battleStation)
+    {
+        for(int i = 0; i < characters.Count; i++)
+        {
+            if(characters[i] != null)
+            {
+                characters[i].gameObject.SetActive(true); 
+                characters[i].transform.SetParent(battleStation, true);
+            }
+            
+        } 
+    }
+
+
+}
